0b6956fa97ef1a33307a0a7dc344160164588089
Emi Tanaka <dr.emi.tanaka@gmail.com>
2025-10-24 12:26:35 +1100

    all data

--- R/data.R
+++ R/data.R
@@ -24,3 +24,19 @@
 #' }
 #' @source Created for demonstration purposes
 "abstracts"
+
+
+#' @rdname abstracts
+"app_2"
+
+#' @rdname abstracts
+"app_3"
+
+#' @rdname abstracts
+"testing_stage_0_data"
+
+#' @rdname abstracts
+"training_stage_0_data"
+
+
+

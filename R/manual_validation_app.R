d24397acc779ede0f2b410af7ecdb1f8cd3cca86
Emi Tanaka <dr.emi.tanaka@gmail.com>
2025-10-29 21:01:39 +1000

    fix manual_validation_app

--- R/manual_validation_app.R
+++ R/manual_validation_app.R
@@ -13,7 +13,7 @@
 #'
 #' @export
 manual_validation_app <- function() {
-  app_dir <- system.file("shiny-app/app.R", package = "metaextractoR")
+  app_dir <- system.file("shiny-app3/app.R", package = "metaextractoR")
 
   shiny::runApp(app_dir, display.mode = "normal")
 }

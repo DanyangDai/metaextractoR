d6df6751c3185fbe65f00a561ff915a5bbc63477
danyangdai <43340871+DanyangDai@users.noreply.github.com>
2025-10-29 16:38:10 +1000

    fix show datatable in prompt enginnering app

--- inst/shiny-app2/app.R
+++ inst/shiny-app2/app.R
@@ -36,6 +36,32 @@ ollama_running <- function() {
 }
 library(shiny)
 
+show_datatable <- function(.data, app_number, edit = FALSE) {
+  if(app_number == 1) {
+    DT::datatable(
+      .data,
+      editable = edit,
+      options = list(
+        dom = 't', # Only show the table, no controls
+        ordering = FALSE,
+        scrolly = '800px',
+        searchHighlight = TRUE
+      ),
+      rownames = FALSE
+    )
+  } else if(app_number == 2) {
+    # uses the same as 1 for now
+  } else if(app_number == 3) {
+    DT::datatable(
+      .data,
+      options = list(dom = 't', ordering = FALSE, pageLength = 50),
+      rownames = FALSE,
+      editable = edit
+    )
+  }
+}
+
+
 # UI ----------------------------------------------------------------------
 ui <- page_sidebar(
   # CHANGED: use bslib page as the top-level (replaces fluidPage)
